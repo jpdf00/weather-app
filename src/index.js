68ddd1a99db6fc163516d2fb7750f65aced221c3
jpdf00 <j.pdf00@gmail.com>
2021-04-28 18:05:47 -0300

    Add form and some style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { getWeather } from './weather';
 
 const API = '2316fb2f745c7ff1232b9e52f05b2491';
-let city = 'london,us'
+let city = 'london,uk'
 let locationURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}`;
 
-const weather = document.createElement('p');
 const content = document.querySelector('#content');
+const weather = document.createElement('p');
 
 getWeather(locationURL)
 .then(function(value) {
@@ -14,6 +14,6 @@ getWeather(locationURL)
   content.appendChild(weather);
 })
 .catch(function(err) {
-  weather.textContent = JSON.stringify('This is not the weather you are looking for!');
+  weather.textContent = 'This is not the weather you are looking for!';
   content.appendChild(weather);
 });
